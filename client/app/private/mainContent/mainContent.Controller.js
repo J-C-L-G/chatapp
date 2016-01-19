15eb3174425617becce23503ba920567d0368b7e
J-C-L-G <ps_jclg@hotmail.com>
2016-01-19 09:12:46 -0800

    Initial - Login Completed
    - When accessing from /login
    -After register immediately login
    
    Working on the UI
    
    -Working in the LeftNav for the Contacts

--- client/app/private/mainContent/mainContent.Controller.js
+++ client/app/private/mainContent/mainContent.Controller.js
@@ -1,4 +1,112 @@
 angular.module('chatApp')
 .controller('mainContent.Controller', function ($scope) {
 
+        var imagePath = 'static/assets/60.jpeg';
+
+        $scope.messages = [{
+            face : imagePath,
+            what: 'Brunch this weekend?',
+            who: 'Min Li Chan',
+            when: '3:08PM',
+            notes: " I'll be in your neighborhood doing errands"
+        }, {
+            face : imagePath,
+            what: 'Brunch this weekend?',
+            who: 'Min Li Chan',
+            when: '3:08PM',
+            notes: " I'll be in your neighborhood doing errands"
+        },
+            {
+                face : imagePath,
+                what: 'Brunch this weekend?',
+                who: 'Min Li Chan',
+                when: '3:08PM',
+                notes: " I'll be in your neighborhood doing errands"
+            },{
+                face : imagePath,
+                what: 'Brunch this weekend?',
+                who: 'Min Li Chan',
+                when: '3:08PM',
+                notes: " I'll be in your neighborhood doing errands"
+            },{
+                face : imagePath,
+                what: 'Brunch this weekend?',
+                who: 'Min Li Chan',
+                when: '3:08PM',
+                notes: " I'll be in your neighborhood doing errands"
+            },{
+                face : imagePath,
+                what: 'Brunch this weekend?',
+                who: 'Min Li Chan',
+                when: '3:08PM',
+                notes: " I'll be in your neighborhood doing errands"
+            },{
+                face : imagePath,
+                what: 'Brunch this weekend?',
+                who: 'Min Li Chan',
+                when: '3:08PM',
+                notes: " I'll be in your neighborhood doing errands"
+            },{
+                face : imagePath,
+                what: 'Brunch this weekend?',
+                who: 'Min Li Chan',
+                when: '3:08PM',
+                notes: " I'll be in your neighborhood doing errands"
+            },{
+                face : imagePath,
+                what: 'Brunch this weekend?',
+                who: 'Min Li Chan',
+                when: '3:08PM',
+                notes: " I'll be in your neighborhood doing errands"
+            },{
+                face : imagePath,
+                what: 'Brunch this weekend?',
+                who: 'Min Li Chan',
+                when: '3:08PM',
+                notes: " I'll be in your neighborhood doing errands"
+            },{
+                face : imagePath,
+                what: 'Brunch this weekend?',
+                who: 'Min Li Chan',
+                when: '3:08PM',
+                notes: " I'll be in your neighborhood doing errands"
+            },{
+                face : imagePath,
+                what: 'Brunch this weekend?',
+                who: 'Min Li Chan',
+                when: '3:08PM',
+                notes: " I'll be in your neighborhood doing errands"
+            },{
+                face : imagePath,
+                what: 'Brunch this weekend?',
+                who: 'Min Li Chan',
+                when: '3:08PM',
+                notes: " I'll be in your neighborhood doing errands"
+            },{
+                face : imagePath,
+                what: 'Brunch this weekend?',
+                who: 'Min Li Chan',
+                when: '3:08PM',
+                notes: " I'll be in your neighborhood doing errands"
+            },
+            {
+            face : imagePath,
+            what: 'Brunch this weekend?',
+            who: 'Min Li Chan',
+            when: '3:08PM',
+            notes: " I'll be in your neighborhood doing errands"
+        }, {
+            face : imagePath,
+            what: 'Brunch this weekend?',
+            who: 'Min Li Chan',
+            when: '3:08PM',
+            notes: " I'll be in your neighborhood doing errands"
+        }, {
+            face : imagePath,
+            what: 'Brunch this weekend?',
+            who: 'Min Li Chan',
+            when: '3:08PM',
+            notes: " I'll be in your neighborhood doing errands"
+        }];
+
 });
--- client/app/private/private.uiRoutes.js
+++ client/app/private/private.uiRoutes.js
@@ -15,12 +15,27 @@ angular.module('chatApp')
                 },
                 'mainContent':{
                     templateUrl:'app/private/mainContent/mainContent.html',
-                    //controller:'mainContent.Controller'
+                    controller:'mainContent.Controller'
                 },
                 'rightNav':{
                     templateUrl:'app/private/rightNav/rightNav.html',
                     //controller:'mainContent.Controller'
                 }
             }
+        })
+        .state('main.inConversation',{
+            url:'/inConversation/:with',
+            views:{
+                'mainContent@':{
+                    templateUrl:'app/private/mainContent/activeConversation.html'
+                }
+            }
         });
+
+            /*
+            .state('main.activeConversation',{
+                url:'/active/:who',
+                templateUrl: 'app/private/mainContent/activeConversation.html'
+            });
+            */
     }]);
